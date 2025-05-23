f2bb4a05de1f566c311d559dba5d2cdda5147a60
ajaynr <ajaynr@netapp.com>
2025-05-23 18:14:55 +0530

    feat: enhance KeyValueList component; add input fields for key, value, and description, and implement select all functionality

--- src/components/KeyValueList.tsx
+++ src/components/KeyValueList.tsx
@@ -1,6 +1,10 @@
 import type { KeyValue } from "../store/slices/RequestSlice";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
+import { IoMdAddCircle } from "react-icons/io";
+import { Input } from "./Input";
+import { Select } from "./Select";
+import { httpHeaderKeys } from "./KeyValueItem";
 
 interface KeyValueItemProps {
     list: KeyValue[];
@@ -14,27 +18,83 @@ export function KeyValueList({list}: KeyValueItemProps) {
 
     const [items, setItems] = useState<ItemsType[]>(list);
 
-    useEffect(() => {
-        setItems(list);
-        console.log('items', items);
-    }, [list]);
-
     const handleCheckboxChange = (index: number) => {
         const updatedItems = [...items];
         updatedItems[index].checked = !updatedItems[index].checked;
+        setItems(updatedItems);
+        console.log("Checkbox changed", updatedItems[index].checked);
+    };
+
+    const handleSelectAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const checked = e.target.checked;
+        const updatedItems = items.map(item => ({ ...item, checked }));
+        setItems(updatedItems);
+        // console.log("Select all", checked);
+        console.log("Updated items", updatedItems);
+    };
+
+    const handleAddClick = () => {
+        const newItem: ItemsType = { key: "", value: "", description: "" };
+        setItems([...items, newItem]);
+    };
+
+    const handleDeleteClick = (index: number) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+    };
+
+    const handleValueChange = (index: number, value: string) => {
+        const updatedItems = [...items];
+        updatedItems[index].value = value;
+        setItems(updatedItems);
+    };
+
+    const handleKeyChange = (index: number, value: string) => {
+        const updatedItems = [...items];
+        updatedItems[index].key = value;
+        setItems(updatedItems);
+    };
+
+    const handleDescriptionChange = (index: number, value: string) => {
+        const updatedItems = [...items];
+        updatedItems[index].description = value;
         setItems(updatedItems);
     };
 
     const renderedKeyValueItems  = items.map((item, index) => {
         return <tr key={index} className="border-b border-gray-200 h-12">
-            <td className="text-left">
-                <input type="checkbox" checked onChange={()=>{handleCheckboxChange(index)}} className="w-4 h-4 cursor-pointer"/>
+            <td className="text-left pl-5 pt-1">
+                <input type="checkbox" onChange={()=>{handleCheckboxChange(index)}} className="w-4 h-4 cursor-pointer"/>
             </td>
-            <td>{item.key}</td>
-            <td>{item.value}</td>
+            <td className="text-left px-5">
+                <Select
+                    options={httpHeaderKeys}
+                    selectedVal={item.key}
+                    onSelect={(value)=>{handleKeyChange(index, value)}}
+                    className="h-8 text-s rounded-sm focus:border-blue-500 focus:ring-blue-500"
+                />
+            </td>
+            <td className="text-left px-5">
+                <Input
+                    placeholder="Value"
+                    className="h-8 text-s rounded-sm focus:border-blue-500 focus:ring-blue-500"
+                    value={item.value}
+                    handleInput={(val) => {handleValueChange(index, val)}}
+                />
+            </td>
             <td>
-                <span className="flex flex-row justify-between mr-20">
-                    <span>{item.description}</span>
+                <span 
+                    onClick={() => handleDeleteClick(index)}
+                    className="flex flex-row justify-between mr-20">
+                    <span className="w-4/5">
+                        <Input
+                            placeholder="Description"
+                            className="h-8 text-s rounded-sm focus:border-blue-500 focus:ring-blue-500"
+                            value={item.description}
+                            handleInput={(val) => {handleDescriptionChange(index, val)}}
+                        />
+                    </span>
                     <span className="text-lg text-gray-500 cursor-pointer self-center mt-1 hover:text-red-500">
                             <AiFillDelete />
                     </span>
@@ -43,11 +103,13 @@ export function KeyValueList({list}: KeyValueItemProps) {
         </tr>;
     });
     return (
-        <div className="flex flex-col gap-2 w-full mx-14">
+        <div className="flex flex-col gap-2 w-full mx-8">
             <table className="w-full">
                 <thead className="border-b border-gray-200 bg-gray-200 h-10">
                     <tr>
-                        <th className="text-left"></th>
+                        <th className="text-left pl-5 pt-1">
+                            <input onChange={handleSelectAll} type="checkbox" className="w-4 h-4 cursor-pointer"/>
+                        </th>
                         <th className="text-left">Key</th>
                         <th className="text-left">Value</th>
                         <th className="text-left">Description</th>
@@ -57,6 +119,14 @@ export function KeyValueList({list}: KeyValueItemProps) {
                     {renderedKeyValueItems}
                 </tbody>
             </table>
+            <div 
+                onClick={handleAddClick}
+                className="flex flex-row gap-1 text-gray-500 text-sm cursor-pointer hover:text-blue-500">
+                Add
+                <span className="text-lg self-center">
+                    <IoMdAddCircle />
+                </span>
+            </div>
         </div>
     );
 }

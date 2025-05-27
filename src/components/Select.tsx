2c206ad0fac6c0e2290f49a3ae0e59e86719a573
ajaynr <ajaynr@netapp.com>
2025-05-27 11:10:12 +0530

    feat: enhance Select component with custom arrow and improved styling for better user experience

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { type JSX } from "react";
 import type { FieldError } from "react-hook-form";
+import { IoIosArrowDown } from "react-icons/io";
 
 interface SelectProps {
     options: string[] | JSX.Element[];
@@ -14,22 +15,27 @@ export function Select({options, selectedVal, className, onSelect, errors,  ...p
     const errorClass = errors ? "border-red-500" : "";
     return (
         <div className={classNames("relative flex items-center", className)}>
-            <select
+            <select 
                 onChange={(e) => onSelect && onSelect(e.target.value)}
                 {...props}
                 className={
                     classNames(
-                    "w-full h-full px-2 bg-white border cursor-pointer border-gray-300 hover:border-blue-200 focus:border-blue-200 focus:ring focus:ring-blue-200 focus:ring-opacity-50",
-                    className, errorClass)
+                        "w-full h-full px-2 pr-8 bg-white border cursor-pointer border-gray-300 hover:border-blue-200 focus:border-blue-200 focus:ring focus:ring-blue-200 focus:ring-opacity-50 appearance-none",
+                        className, errorClass
+                    )
                 }
                 defaultValue={selectedVal}
             >
-                {options.map((option,index) => (
+                {options.map((option, index) => (
                     <option key={index}>
                         {option}
                     </option>
                 ))}
             </select>
+            {/* Custom arrow */}
+            <span className="pointer-events-none absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+                <IoIosArrowDown/>
+            </span>
             {
                 errors && 
                 <span className="text-red-500 text-xs">
